179d8f2596e4047948b8ced6c754fc64d93e9e17
Luceliosantos <69224783+Luceliosantos@users.noreply.github.com>
2026-03-26 08:37:57 -0300

    Update Consulta.tsx

--- src/pages/Consulta.tsx
+++ src/pages/Consulta.tsx
@@ -455,11 +455,26 @@ export default function Consulta({
                         (valor, i) => (
 
                           <td
-                            key={i}
-                            style={styles.td}
-                          >
-                            {String(valor)}
-                          </td>
+  key={i}
+  style={styles.td}
+>
+  {
+    typeof valor === "string" &&
+    valor.includes("T") &&
+    !isNaN(Date.parse(valor))
+      ? new Date(valor)
+          .toLocaleString("pt-BR", {
+            day:"2-digit",
+            month:"2-digit",
+            year:"numeric",
+            hour:"2-digit",
+            minute:"2-digit",
+            second:"2-digit"
+          })
+          .replace(",", " -")
+      : String(valor)
+  }
+</td>
 
                         )
                       )}
