c07e86ea1ca2fd2083b7918d08d055e31bcb6195
Luceliosantos <69224783+Luceliosantos@users.noreply.github.com>
2026-04-16 19:08:38 -0300

    Update CorrigirCadastro.tsx

--- src/pages/CorrigirCadastro.tsx
+++ src/pages/CorrigirCadastro.tsx
@@ -257,8 +257,16 @@ export default function CorrigirCadastro({
                       item.dt_ass_db
                       ?
 
-                      new Date(item.dt_ass_db)
-                      .toLocaleString("pt-BR")
+new Date(item.dt_ass_db)
+.toLocaleString("pt-BR",{
+  day:"2-digit",
+  month:"2-digit",
+  year:"numeric",
+  hour:"2-digit",
+  minute:"2-digit",
+  second:"2-digit"
+})
+.replace(","," - ")
 
                       :
 
