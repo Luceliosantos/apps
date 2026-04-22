53a88a3e4e9c9be0ad3d1cf443e5e617ce9c7317
Luceliosantos <69224783+Luceliosantos@users.noreply.github.com>
2026-04-22 17:38:09 -0300

    Add ProorcKits component with form and editor

---
+++ src/components/proorc-admin/ProorcKits.tsx
@@ -0,0 +1,11 @@
+import ProorcKitForm from "./ProorcKitForm";
+import ProorcKitEditor from "./ProorcKitEditor";
+
+export default function ProorcKits() {
+  return (
+    <div>
+      <ProorcKitForm />
+      <ProorcKitEditor />
+    </div>
+  );
+}
