a57a490612dba34ce4fea54e190499fc7ef785a7
Luceliosantos <69224783+Luceliosantos@users.noreply.github.com>
2026-04-23 11:23:38 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "./supabase";
+import { supabase } from './lib/supabase'
 
 import ControleChaves from "./pages/ControleChaves";
 import Cadastro from "./pages/Cadastro";
