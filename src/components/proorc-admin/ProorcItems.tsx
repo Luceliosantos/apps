2484d8db1d77a9137f25fbfef1bcb51533cc3e5e
Luceliosantos <69224783+Luceliosantos@users.noreply.github.com>
2026-04-23 08:27:39 -0300

    Update ProorcItems.tsx

--- src/components/proorc-admin/ProorcItems.tsx
+++ src/components/proorc-admin/ProorcItems.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../lib/supabase";
 import ProorcItemForm from "./ProorcItemForm";
 
 export default function ProorcItems() {
